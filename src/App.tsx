b57a8efcfb6ce86c11387e014875b6169f63fdcf
Ryan Hall <hall.ryan.r@gmail.com>
2020-07-02 09:05:19 -0600

    convert to pizza club

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { LinkTokenFactory } from '@chainlink/contracts/ethers/v0.4/LinkTokenFactory'
+import { ethers } from 'ethers'
+
+// @ts-ignore
+window.ethereum.enable()
+// @ts-ignore
+const provider = new ethers.providers.Web3Provider(window.web3.currentProvider)
+const signer = provider.getSigner()
+
+const linkTokenFactory = new LinkTokenFactory(signer)
+
+const enroll = async () => {
+  const address = await signer.getAddress()
+  const transferAndCallData =
+    // @ts-ignore
+    web3.sha3('onTokenTransfer(address,uint256,bytes)').substring(0, 10) +
+    ethers.utils.AbiCoder.prototype
+      .encode(['address', 'uint256'], [address, 25])
+      .slice(2)
+
+  await linkTokenFactory
+    .connect(signer)
+    .attach('0xa36085f69e2889c224210f603d836748e7dc0088')
+    .transferAndCall(await signer.getAddress(), 25, transferAndCallData)
+}
 
 function App() {
+  const [enrolled, setEnrolled] = useState(false)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {enrolled ? <Enrolled /> : <Enroll setEnrolled={setEnrolled} />}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+function Enroll({ setEnrolled }: any) {
+  const handleEnroll = async () => {
+    await enroll()
+    setEnrolled(true)
+  }
+  return (
+    <>
+      <p>One Hundred Dollar Pizza Club</p>
+      <p>
+        <button className="App-button" onClick={handleEnroll}>
+          Enroll
+        </button>
+      </p>
+    </>
+  )
+}
+
+function Enrolled() {
+  return (
+    <>
+      <p>Welcome to the club :)</p>
+      <p>Your first pizza is on the way!</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+declare global {
+  /*~ Here, declare things that go in the global namespace, or augment
+   *~ existing declarations in the global namespace
+   */
+  interface web3 {}
+}
